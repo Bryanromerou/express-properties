c30aabc2f7041c88eec1f6e50bf2bdc873dff5c3
Bryanromerou <Bryanromerou@gmail.com>
2020-10-01 14:21:28 -0700

    added Property Controller

--- server.js
+++ server.js
@@ -3,7 +3,11 @@ const express = require("express");
 const app = express();
 const bodyParser = require('body-parser');
 
+
 //Middleware
 app.use(bodyParser.urlencoded({ extended: false }));
 app.set("view engine", "ejs");
 
+//Property Controller
+const propertyCtrl = require("./controllers/propertyController");
+app.use("/properties",propertyCtrl);
