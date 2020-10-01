3c2d6b2a038bc7ac7f6ea0e45ed7131551d824b8
Bryanromerou <Bryanromerou@gmail.com>
2020-10-01 14:25:16 -0700

    Added a home index

--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 const express = require("express");
 const app = express();
 const bodyParser = require('body-parser');
+const PORT = 4000;
 
 
 //Middleware
@@ -11,3 +12,13 @@ app.set("view engine", "ejs");
 //Property Controller
 const propertyCtrl = require("./controllers/propertyController");
 app.use("/properties",propertyCtrl);
+
+//Home
+app.get("/",(req,res)=>{
+    res.send("This is Home");
+
+})
+
+app.listen(PORT, ()=>{
+    console.log("Now listening to port 4000");
+});
