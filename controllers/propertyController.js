e1a663aabc2653132f9907e0bb513fa8760d3bc2
Bryanromerou <Bryanromerou@gmail.com>
2020-10-01 22:19:08 -0700

    Completed update route

--- controllers/propertyController.js
+++ controllers/propertyController.js
@@ -1,8 +1,58 @@
 const express = require("express");
 const router = express.Router();
 
+const properties = require("../models/properties");
+
+//Index
 router.get("/",(req,res)=>{
-    res.send("This is the property index");
+    console.log(properties);
+    res.render("properties/indexProperty",{
+        properties: properties,
+    });
 });
 
+//Show
+router.get("/:propertyIndex",(req,res)=>{
+    const propertyIndex = req.params.propertyIndex;
+    const property = properties[propertyIndex];
+
+    res.render("properties/showProperty",{
+        property: property,
+        propertyIndex: propertyIndex
+    });
+});
+
+//Edit
+router.get("/:propertyIndex/edit",(req,res)=>{
+    const propertyIndex = req.params.propertyIndex;
+    const property = properties[propertyIndex];
+
+    res.render("properties/editProperty",{
+        property: property,
+        propertyIndex: propertyIndex
+    });
+});
+
+//Destroy
+router.delete("/:propertyIndex",(req,res)=>{
+    const propertyIndex = req.params.propertyIndex;
+    properties.splice(propertyIndex,1);
+    res.redirect(`/properties/${propertyIndex}`);
+});
+
+//Update
+router.put("/:propertyIndex",(req,res) => {
+    const propertyIndex = req.params.propertyIndex;
+    const newFruit = {
+        address: req.body.address,
+        price: req.body.price,
+        bedrooms: req.body.bedrooms,
+        lot: req.body.lot,
+        img: req.body.img,
+    }
+    properties[propertyIndex] = newFruit;
+    // properties.splice(propertyIndex,1,newFruit);
+    res.redirect(`/properties/${propertyIndex}`);
+
+});
 module.exports = router;
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 //Constant Variables
 const express = require("express");
 const app = express();
+const methodOverride = require('method-override')
 const bodyParser = require('body-parser');
 const PORT = 4000;
 
@@ -8,6 +9,7 @@ const PORT = 4000;
 //Middleware
 app.use(bodyParser.urlencoded({ extended: false }));
 app.set("view engine", "ejs");
+app.use(methodOverride('_method'));
 
 //Property Controller
 const propertyCtrl = require("./controllers/propertyController");
@@ -15,8 +17,7 @@ app.use("/properties",propertyCtrl);
 
 //Home
 app.get("/",(req,res)=>{
-    res.send("This is Home");
-
+    res.render("index");
 })
 
 app.listen(PORT, ()=>{
